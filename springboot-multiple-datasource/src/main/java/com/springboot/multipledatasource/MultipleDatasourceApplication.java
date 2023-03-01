1777e1966e3fa281b0b45b3be01445bb9c804803
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-03-02 01:00:10 +0700

    test: add testcase

--- springboot-multiple-datasource/src/main/java/com/springboot/multipledatasource/MultipleDatasourceApplication.java
+++ springboot-multiple-datasource/src/main/java/com/springboot/multipledatasource/MultipleDatasourceApplication.java
@@ -1,24 +1,13 @@
 package com.springboot.multipledatasource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.springboot.multipledatasource.service.DataService;
 
 @SpringBootApplication
-public class MultipleDatasourceApplication implements CommandLineRunner {
-
-  @Autowired
-  private DataService dataService;
+public class MultipleDatasourceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MultipleDatasourceApplication.class, args);
-  }
-
-  @Override
-  public void run(String... args) throws Exception {
-    dataService.manipulate();
   }
 
 }
---
+++ springboot-multiple-datasource/src/test/java/com/springboot/multipledatasource/MultipleDatasourceApplicationIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.springboot.multipledatasource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import com.springboot.multipledatasource.vehicle.CarEntity;
+import com.springboot.multipledatasource.vehicle.CardRepository;
+import com.springboot.multipledatasource.wild.AnimalEntity;
+import com.springboot.multipledatasource.wild.AnimalRepository;
+
+@DataJpaTest
+class MultipleDatasourceApplicationIntegrationTest {
+
+  @Autowired
+  CardRepository cardRepository;
+
+  @Autowired
+  AnimalRepository animalRepository;
+
+  @Test
+  void testVehicleDatabase() {
+
+    var car = new CarEntity();
+    car.setName("BUS");
+    var newCar = cardRepository.save(car);
+    Optional<CarEntity> result = cardRepository.findById(newCar.getId());
+    assertThat(result).isPresent();
+  }
+
+  @Test
+  void testWildDatabase() {
+
+    var animal = new AnimalEntity();
+    animal.setName("Jerry");
+    var newAnimal = animalRepository.save(animal);
+    Optional<AnimalEntity> result = animalRepository.findById(newAnimal.getId());
+    assertThat(result).isPresent();
+  }
+
+}
