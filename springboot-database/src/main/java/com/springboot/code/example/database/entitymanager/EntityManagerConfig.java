3ab4f21265c84a62b84a100fa55d267420ef04df
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-04-27 08:23:19 +0700

    act: remove ModelMapper

--- springboot-database/src/main/java/com/springboot/code/example/database/entitymanager/EntityManagerConfig.java
+++ springboot-database/src/main/java/com/springboot/code/example/database/entitymanager/EntityManagerConfig.java
@@ -1,17 +1,11 @@
 package com.springboot.code.example.database.entitymanager;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class EntityManagerConfig {
-
-  @Bean
-  ModelMapper modelMapper() {
-    return new ModelMapper();
-  }
 
   @Bean
   ObjectMapper objectMapper() {
