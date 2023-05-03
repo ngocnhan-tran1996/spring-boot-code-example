b488834e033a4375c1db3274dea808a453b78f20
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-05-03 13:18:37 +0700

    act: update script

--- springboot-database/src/main/resources/jdbc/3.1_execute_pack_example.sql
+++ springboot-database/src/main/resources/jdbc/3.1_execute_pack_example.sql
@@ -1,17 +1,3 @@
--- execute user_nhan.create_car_table_proc
-DECLARE
-    in_schema dba_tables.owner%TYPE;
-    in_table_name dba_tables.table_name%TYPE;
-    out_msg VARCHAR2(100);
-BEGIN
-    in_schema := 'user_nhan';
-    in_table_name := 'car';
-    user_nhan.create_car_table_proc (
-        in_schema => in_schema,
-        in_table_name => in_table_name,
-        out_msg => out_msg);
-END;
-
 -- execute pack_example.concatenate_text_proc
 DECLARE
   IN_NAME VARCHAR2(200);
