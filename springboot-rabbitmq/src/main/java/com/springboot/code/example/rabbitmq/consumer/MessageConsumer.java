8bc079df0612cf79a246d426e7a096ad8c2ac33a
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-05-19 03:51:02 +0700

    act: create queue example

--- springboot-rabbitmq/src/main/java/com/springboot/code/example/rabbitmq/consumer/MessageConsumer.java
+++ springboot-rabbitmq/src/main/java/com/springboot/code/example/rabbitmq/consumer/MessageConsumer.java
@@ -9,16 +9,14 @@ import lombok.extern.log4j.Log4j2;
 @Service
 public class MessageConsumer {
 
-  @RabbitListener(queues = {
-      "local_QUEUE_TEST",
-      "local_QUEUE_TEST_1",
-      "local_QUEUE_TEST_2",
-      "local_QUEUE_TEST_3",
-      "local_QUEUE_TEST_4"
-  })
-  void receive(Message message) {
+  @RabbitListener(queues = "#{'${app.queue.names}'.split(',')}")
+  public void receive(Message message) {
 
-    log.info("{}", new String(message.getBody()));
+    var messageProperties = message.getMessageProperties();
+    log.info("Receive message {} from queue {} with exchange {}",
+        new String(message.getBody()),
+        messageProperties.getConsumerQueue(),
+        messageProperties.getReceivedExchange());
   }
 
 }
