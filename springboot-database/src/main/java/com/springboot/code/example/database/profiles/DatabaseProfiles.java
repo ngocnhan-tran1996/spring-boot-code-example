6b8f21ab31e135ea7867d7bd9339d4d6275bad80
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-05-26 01:49:56 +0700

    act: log sql

--- springboot-database/src/main/java/com/springboot/code/example/database/profiles/DatabaseProfiles.java
+++ springboot-database/src/main/java/com/springboot/code/example/database/profiles/DatabaseProfiles.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class DatabaseProfiles {
 
-  public static final String MULTIPLE_DATASOURCE = "multiple-datasource";
+  public static final String MULTIPLE_DATASOURCE = "multiple.datasource";
   public static final String ENTITY_MANAGER = "entitymanager";
   public static final String JDBC = "jdbc";
 
