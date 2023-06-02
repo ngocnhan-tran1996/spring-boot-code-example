80f65499cc7ce09911a049eafe7bbc4a6298a941
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-06-03 01:29:23 +0700

    act: remove init method

--- springboot-database/springboot-database-entity-manager/src/main/java/com/springboot/code/example/database/entity/manager/EntityManagerApplication.java
+++ springboot-database/springboot-database-entity-manager/src/main/java/com/springboot/code/example/database/entity/manager/EntityManagerApplication.java
@@ -1,30 +1,13 @@
 package com.springboot.code.example.database.entity.manager;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import com.springboot.code.example.database.entity.manager.example.EntityManagerExample;
-import lombok.extern.log4j.Log4j2;
 
-@Log4j2
 @SpringBootApplication
 public class EntityManagerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(EntityManagerApplication.class, args);
-  }
-
-  @Autowired
-  EntityManagerExample entityManagerExample;
-
-  @Bean
-  CommandLineRunner commandLineRunner() {
-    return args -> {
-      log.info("query {}", entityManagerExample.toNativeQuery());
-      log.info("query {}", entityManagerExample.saveAndFindAll());
-    };
   }
 
 }
--- springboot-database/springboot-database-multiple-datasource/src/main/java/com/springboot/code/example/database/multiple/datasource/MultipleDatasourceApplication.java
+++ springboot-database/springboot-database-multiple-datasource/src/main/java/com/springboot/code/example/database/multiple/datasource/MultipleDatasourceApplication.java
@@ -1,29 +1,13 @@
 package com.springboot.code.example.database.multiple.datasource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import com.springboot.code.example.database.multiple.datasource.example.MultipleDatasourceExample;
-import lombok.extern.log4j.Log4j2;
 
-@Log4j2
 @SpringBootApplication
 public class MultipleDatasourceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MultipleDatasourceApplication.class, args);
-  }
-
-  @Autowired
-  MultipleDatasourceExample multipleDatasourceExample;
-
-  @Bean
-  CommandLineRunner commandLineRunner() {
-    return args -> {
-      log.info("query {}", multipleDatasourceExample.getAllBaseEntity());
-    };
   }
 
 }
