8b7b53ecf65b0a4e1b5dd990c263dd5eed6bd000
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-06-04 00:21:44 +0700

    act: remove unused

--- springboot-database/springboot-database-entity-manager/src/main/java/com/springboot/code/example/database/entity/manager/response/CarResponse.java
+++ springboot-database/springboot-database-entity-manager/src/main/java/com/springboot/code/example/database/entity/manager/response/CarResponse.java
@@ -3,10 +3,8 @@ package com.springboot.code.example.database.entity.manager.response;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 @EqualsAndHashCode
 public class CarResponse {
