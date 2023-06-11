1d2552058769e46f3833d9588b3aa59a5c1c7963
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-06-11 19:04:39 +0700

    act: update script

--- springboot-database/springboot-database-entity-manager/src/main/java/com/springboot/code/example/database/entity/manager/response/CarResponse.java
+++ springboot-database/springboot-database-entity-manager/src/main/java/com/springboot/code/example/database/entity/manager/response/CarResponse.java
@@ -1,12 +1,10 @@
 package com.springboot.code.example.database.entity.manager.response;
 
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-@EqualsAndHashCode
 public class CarResponse {
 
   private int id;
