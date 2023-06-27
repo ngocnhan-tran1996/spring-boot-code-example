ad173d208f131c37173e5f7a62c1a85c7c8b34d5
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-06-27 23:43:34 +0700

    act: add test log4j2

--- springboot-log4j2/src/test/java/com/springboot/code/example/log4j2/consumer/MessageConsumerTests.java
+++ springboot-log4j2/src/test/java/com/springboot/code/example/log4j2/consumer/MessageConsumerTests.java
@@ -1,80 +1,65 @@
 package com.springboot.code.example.log4j2.consumer;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Appender;
-import org.apache.logging.log4j.core.LogEvent;
-import org.apache.logging.log4j.core.Logger;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import lombok.extern.log4j.Log4j2;
 
-@ExtendWith(MockitoExtension.class)
+@Log4j2
+@SpringBootTest
+@ExtendWith(OutputCaptureExtension.class)
 class MessageConsumerTests {
 
-  @Mock
-  private Appender mockAppender;
+  private static final List<String> OUTPUT = List.of(
+      "\"password\" = \"test\"",
+      "\"password\"=\"test\"",
+      "\"password\"= \"test\"",
+      "\"password\" =\"test\"",
 
-  @Captor
-  private ArgumentCaptor<LogEvent> captorLoggingEvent;
+      "\\\"password\\\" = \\\"test\\\"",
+      "\"password\"=\\\"test\\\"",
+      "\\\"password\\\"= \\\"test\\\"",
+      "\\\"password\\\" =\\\"test\\\"",
 
-  private Logger logger;
+      "\"password\" : \"test\"",
+      "\"password\":\"test\"",
+      "\"password\": \"test\"",
+      "\"password\" :\"test\"",
 
-  @BeforeEach
-  public void setup() {
-    // prepare the appender so Log4j likes it
-    when(mockAppender.getName()).thenReturn("MockAppender");
-    when(mockAppender.isStarted()).thenReturn(true);
-    when(mockAppender.isStopped()).thenReturn(false);
- 
-    logger = (Logger)LogManager.getLogger(MessageConsumerTests.class);
-    logger.addAppender(mockAppender);
-    logger.setLevel(Level.INFO);
-  }
+      "\\\"password\\\" : \\\"test\\\"",
+      "\"password\":\\\"test\\\"",
+      "\\\"password\\\": \\\"test\\\"",
+      "\\\"password\\\" :\\\"test\\\"");
+  private static final List<String> MASK_OUTPUT = List.of(
+      "\"password\" = \"****\"",
+      "\"password\"=\"****\"",
+      "\"password\"= \"****\"",
+      "\"password\" =\"****\"",
 
-  @AfterEach
-  public void tearDown() {
-    // the appender we added will sit in the singleton logger forever
-    // slowing future things down - so remove it
-    logger.removeAppender(mockAppender);
-  }
+      "\\\"password\\\" = \\\"****\\\"",
+      "\"password\"=\\\"****\\\"",
+      "\\\"password\\\"= \\\"****\\\"",
+      "\\\"password\\\" =\\\"****\\\"",
+
+      "\"password\" : \"****\"",
+      "\"password\":\"****\"",
+      "\"password\": \"****\"",
+      "\"password\" :\"****\"",
+
+      "\\\"password\\\" : \\\"****\\\"",
+      "\"password\":\\\"****\\\"",
+      "\\\"password\\\": \\\"****\\\"",
+      "\\\"password\\\" :\\\"****\\\"");
 
   @Test
-  public void loggingIsCaptured() {
-    logger.error("What an error");
-    verifyErrorMessages("What an error");
-  }
+  void testMaskLog4j2(CapturedOutput capturedOutput) {
 
-  @Test
-  public void noBleedBetweenLogCaptures() {
-    logger.error("What an oopsie");
-    verifyErrorMessages("What an oopsie");
-  }
-
-  @Test
-  public void nothingLogged() {
-    verifyErrorMessages();
-  }
-
-  // handy function to inspect the messages sent to the logger
-  private void verifyErrorMessages(String... messages) {
-    verify(mockAppender, times(messages.length))
-        .append(captorLoggingEvent.capture());
-
-    int i = 0;
-    for (LogEvent loggingEvent : captorLoggingEvent.getAllValues()) {
-      assertEquals(messages[i++],
-          loggingEvent.getMessage().getFormattedMessage());
-    }
+    OUTPUT.forEach(log::info);
+    assertThat(capturedOutput.getOut()).contains(MASK_OUTPUT.toArray(String[]::new));
   }
 
 }
