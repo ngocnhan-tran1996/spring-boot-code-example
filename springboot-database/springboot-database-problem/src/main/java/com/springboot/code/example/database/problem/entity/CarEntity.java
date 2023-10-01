834652eebf19cadc9ab61fbf6e4df043842afe3a
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-10-01 18:00:38 +0700

    act: remove unused import

--- springboot-database/springboot-database-problem/src/main/java/com/springboot/code/example/database/problem/entity/CarEntity.java
+++ springboot-database/springboot-database-problem/src/main/java/com/springboot/code/example/database/problem/entity/CarEntity.java
@@ -6,17 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "CAR")
-@NoArgsConstructor
-@AllArgsConstructor
 public class CarEntity {
 
   @Id
