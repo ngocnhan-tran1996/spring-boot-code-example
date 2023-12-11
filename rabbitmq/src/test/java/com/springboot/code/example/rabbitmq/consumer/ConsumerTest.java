f818ffa221d759e01a24e0f2a1e995e77b5985a0
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-12-11 22:56:58 +0700

    test: add case

--- rabbitmq/src/test/java/com/springboot/code/example/rabbitmq/consumer/ConsumerTest.java
+++ rabbitmq/src/test/java/com/springboot/code/example/rabbitmq/consumer/ConsumerTest.java
@@ -2,7 +2,9 @@ package com.springboot.code.example.rabbitmq.consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.QueueInformation;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,10 +26,24 @@ class ConsumerTest {
   @Test
   void testConsume() throws Exception {
 
+    // auto create queue
     rabbitTemplate.convertAndSend(ConsumerListener.QUEUE_NAME, "Test");
     var queueInfo = rabbitAdmin.getQueueInfo(ConsumerListener.QUEUE_NAME);
     assertThat(queueInfo.getConsumerCount()).isEqualTo(1);
     assertThat(queueInfo.getMessageCount()).isZero();
+
+    // delete queue by manual
+    rabbitAdmin.deleteQueue(ConsumerListener.QUEUE_NAME);
+    queueInfo = rabbitAdmin.getQueueInfo(ConsumerListener.QUEUE_NAME);
+    assertThat(queueInfo).isNull();
+
+    latch.await(1, TimeUnit.SECONDS);
+
+    // auto re-create queue
+    queueInfo = rabbitAdmin.getQueueInfo(ConsumerListener.QUEUE_NAME);
+    assertThat(queueInfo)
+        .usingRecursiveComparison()
+        .isEqualTo(new QueueInformation(ConsumerListener.QUEUE_NAME, 0, 0));
   }
 
 }
