2823f296173d40320d38cc084081948034c94eb9
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-12-17 13:53:08 +0700

    test: add case json

---
+++ rabbitmq/src/test/java/com/springboot/code/example/rabbitmq/json/JsonListenerConfig.java
@@ -0,0 +1,21 @@
+package com.springboot.code.example.rabbitmq.json;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+class JsonListenerConfig {
+
+  @Bean
+  JsonListener listener() {
+
+    return new JsonListener();
+  }
+
+  @Bean
+  Jackson2JsonMessageConverter jsonConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
+
+}
