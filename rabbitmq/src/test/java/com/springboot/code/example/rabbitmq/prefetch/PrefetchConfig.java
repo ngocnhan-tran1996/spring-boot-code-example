a92673846445dc76f0d3c3f38f1670aa13b0744e
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-12-22 18:20:03 +0700

    test: add case prefetch

---
+++ rabbitmq/src/test/java/com/springboot/code/example/rabbitmq/prefetch/PrefetchConfig.java
@@ -0,0 +1,40 @@
+package com.springboot.code.example.rabbitmq.prefetch;
+
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+class PrefetchConfig {
+
+  @Bean("roundRobinContainerFactory")
+  SimpleRabbitListenerContainerFactory roundRobinContainerFactory(
+      SimpleRabbitListenerContainerFactoryConfigurer configurer,
+      ConnectionFactory connectionFactory) {
+
+    var factory = new SimpleRabbitListenerContainerFactory();
+    configurer.configure(factory, connectionFactory);
+    factory.setPrefetchCount(250);
+    return factory;
+  }
+
+  @Bean("fairContainerFactory")
+  SimpleRabbitListenerContainerFactory fairContainerFactory(
+      SimpleRabbitListenerContainerFactoryConfigurer configurer,
+      ConnectionFactory connectionFactory) {
+
+    var factory = new SimpleRabbitListenerContainerFactory();
+    configurer.configure(factory, connectionFactory);
+    factory.setPrefetchCount(1);
+    return factory;
+  }
+
+  @Bean
+  PrefetchListener prefetchListener() {
+
+    return new PrefetchListener();
+  }
+
+}
---
+++ rabbitmq/src/test/java/com/springboot/code/example/rabbitmq/prefetch/PrefetchListener.java
@@ -0,0 +1,27 @@
+package com.springboot.code.example.rabbitmq.prefetch;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+class PrefetchListener {
+
+  final CountDownLatch latch = new CountDownLatch(1);
+
+  @RabbitListener(queues = "#{queue.name}", containerFactory = "roundRobinContainerFactory")
+  void consume1(String msg) throws InterruptedException {
+
+    log.info("Consumer 1 : {}", msg);
+    latch.await(1, TimeUnit.MINUTES);
+  }
+
+  @RabbitListener(queues = "#{queue.name}", containerFactory = "fairContainerFactory")
+  void consume2(String msg) throws InterruptedException {
+
+    log.info("Consumer 2 : {}", msg);
+    latch.await(1, TimeUnit.MINUTES);
+  }
+
+}
