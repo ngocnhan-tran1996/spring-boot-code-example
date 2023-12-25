e357e3aa3249078269f85fc2c7b33866fc8a0d83
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-12-26 01:36:26 +0700

    test: using createNativeQuery

--- database/src/test/java/com/springboot/code/example/database/domain/name/NamePrefixResponse.java
+++ database/src/test/java/com/springboot/code/example/database/domain/name/NamePrefixResponse.java
@@ -1,17 +1,7 @@
 package com.springboot.code.example.database.domain.name;
 
 import java.math.BigDecimal;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-public class NamePrefixResponse {
-
-  private String firstName;
-  private String lastName;
-  private BigDecimal age;
+public record NamePrefixResponse(String firstName, String lastName, BigDecimal age) {
 
 }
