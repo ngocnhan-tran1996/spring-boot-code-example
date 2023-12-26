e03707bca072606c07c7fab3326f9f142f683c71
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-12-26 19:37:30 +0700

    act: fix for 3.1.x

--- database/src/test/java/com/springboot/code/example/database/entitymanager/TypedQueryPaginationService.java
+++ database/src/test/java/com/springboot/code/example/database/entitymanager/TypedQueryPaginationService.java
@@ -13,8 +13,8 @@ import org.hibernate.query.sqm.tree.select.SqmSelectStatement;
 import org.hibernate.sql.ast.SqlAstTranslator;
 import org.hibernate.sql.ast.spi.StandardSqlAstTranslatorFactory;
 import org.hibernate.sql.ast.tree.select.SelectStatement;
+import org.hibernate.sql.exec.spi.JdbcOperationQuerySelect;
 import org.hibernate.sql.exec.spi.JdbcParameterBinder;
-import org.hibernate.sql.exec.spi.JdbcSelect;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.springboot.code.example.database.domain.name.NamePrefixEntity;
@@ -88,9 +88,10 @@ public class TypedQueryPaginationService {
             querySqmImpl.getSession().getLoadQueryInfluencers(),
             sessionFactory,
             true);
-    final SqlAstTranslator<JdbcSelect> sqlAstTranslator = new StandardSqlAstTranslatorFactory()
-        .buildSelectTranslator(sessionFactory, sqmTranslator.translate().getSqlAst());
-    final JdbcSelect jdbcSelect = sqlAstTranslator.translate(null, queryOptions);
+    final SqlAstTranslator<JdbcOperationQuerySelect> sqlAstTranslator =
+        new StandardSqlAstTranslatorFactory()
+            .buildSelectTranslator(sessionFactory, sqmTranslator.translate().getSqlAst());
+    final JdbcOperationQuerySelect jdbcSelect = sqlAstTranslator.translate(null, queryOptions);
 
     // arrange params
     final List<JdbcParameterBinder> parameterBinders = jdbcSelect.getParameterBinders();
@@ -102,7 +103,7 @@ public class TypedQueryPaginationService {
             Function.identity()));
 
     // set params
-    var nativeQuery = this.entityManager.createNativeQuery(jdbcSelect.getSql());
+    var nativeQuery = this.entityManager.createNativeQuery(jdbcSelect.getSqlString());
     sqmTranslator.getJdbcParamsBySqmParam()
         .entrySet()
         .stream()
