e70438dc8a799fb457ddb18fca3a3e17bb197e99
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-12-31 00:28:47 +0700

    act: fix for 2.7.x

--- transaction/src/test/java/com/springboot/code/example/transaction/domain/DogEntity.java
+++ transaction/src/test/java/com/springboot/code/example/transaction/domain/DogEntity.java
@@ -1,10 +1,9 @@
 package com.springboot.code.example.transaction.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
