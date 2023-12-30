d5cf9c6f9479553b0cfe306cb5f18d9bc026bf34
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2023-12-30 15:55:28 +0700

    act: add transaction module

---
+++ transaction/src/test/java/com/springboot/code/example/rabbitmq/transaction/TransactionProducer.java
@@ -0,0 +1,22 @@
+package com.springboot.code.example.rabbitmq.transaction;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Transactional
+@RequiredArgsConstructor
+class TransactionProducer {
+
+  private final Queue queue;
+  private final RabbitTemplate rabbitTemplate;
+
+  public void send(boolean hasTransaction) {
+
+    rabbitTemplate.setChannelTransacted(hasTransaction);
+    this.rabbitTemplate.convertAndSend(queue.getName(), "test");
+    throw new RuntimeException();
+  }
+
+}
---
+++ transaction/src/test/java/com/springboot/code/example/transaction/domain/DogEntity.java
@@ -0,0 +1,29 @@
+package com.springboot.code.example.transaction.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "dog")
+public class DogEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  private String species;
+
+  public DogEntity(String species) {
+
+    this.species = species;
+  }
+
+}
