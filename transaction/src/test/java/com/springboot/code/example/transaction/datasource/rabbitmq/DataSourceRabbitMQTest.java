5481273bbd7f29ab3e6233546f53be502777ef1f
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2024-01-01 02:20:22 +0700

    test: add entityManager

--- transaction/src/test/java/com/springboot/code/example/transaction/datasource/rabbitmq/DataSourceRabbitMQTest.java
+++ transaction/src/test/java/com/springboot/code/example/transaction/datasource/rabbitmq/DataSourceRabbitMQTest.java
@@ -3,16 +3,24 @@ package com.springboot.code.example.transaction.datasource.rabbitmq;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import com.springboot.code.example.container.EnableTestcontainers;
 import com.springboot.code.example.container.PostgreSQLContainerInitializer;
 import com.springboot.code.example.container.RabbitMQContainerInitializer;
 import com.springboot.code.example.testcase.TestCase;
+import com.springboot.code.example.transaction.domain.DogRepo;
 import com.springboot.code.example.transaction.rabbitmq.BaseConfig;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TransactionRequiredException;
 
 @ActiveProfiles("postgres")
 @SpringBootTest(classes = {
@@ -20,6 +28,7 @@ import com.springboot.code.example.transaction.rabbitmq.BaseConfig;
     DataSourceRabbitMQConfig.class
 })
 @EnableTestcontainers({PostgreSQLContainerInitializer.class, RabbitMQContainerInitializer.class})
+@TestMethodOrder(OrderAnnotation.class)
 class DataSourceRabbitMQTest {
 
   @Autowired
@@ -31,30 +40,99 @@ class DataSourceRabbitMQTest {
   @Autowired
   DataSourceRabbitMQConfigProducer producer;
 
+  @Autowired
+  DogRepo dogRepo;
+
+  @Autowired
+  JdbcTemplate jdbcTemplate;
+
+  @Autowired
+  EntityManager entityManager;
+
   @BeforeEach
   void init() {
 
     this.rabbitAdmin.purgeQueue(this.queue.getName());
+    dogRepo.deleteAll();
   }
 
+  @Order(1)
   @TestCase("com.springboot.code.example.transaction.rabbitmq.testcase.TransactionTestArguments#testSendArguments")
-  void testSend(boolean input, int output) throws Exception {
+  void testSendWithJpa(boolean input, int output) throws Exception {
 
     assertThatExceptionOfType(RuntimeException.class)
         .isThrownBy(() -> producer.send(input));
     var msgCount = rabbitAdmin.getQueueInfo(queue.getName())
         .getMessageCount();
     assertThat(msgCount).isEqualTo(output);
+    assertThat(dogRepo.findAll())
+        .isEmpty();
   }
 
+  @Order(2)
   @TestCase("com.springboot.code.example.transaction.rabbitmq.testcase.TransactionTestArguments#testSendWithCloneArguments")
-  void testSendWithoutTransaction(boolean input, int output) throws Exception {
+  void testSendWithJpaWithoutTransaction(boolean input, int output) throws Exception {
 
     assertThatExceptionOfType(RuntimeException.class)
         .isThrownBy(() -> producer.sendWithoutTransaction(input));
     var msgCount = rabbitAdmin.getQueueInfo(queue.getName())
         .getMessageCount();
     assertThat(msgCount).isEqualTo(output);
+    assertThat(dogRepo.findAll())
+        .hasSize(output);
+  }
+
+  @Order(3)
+  @TestCase("com.springboot.code.example.transaction.rabbitmq.testcase.TransactionTestArguments#testSendArguments")
+  void testSendJdbc(boolean input, int output) throws Exception {
+
+    assertThatExceptionOfType(RuntimeException.class)
+        .isThrownBy(() -> producer.sendJdbc(input));
+    var msgCount = rabbitAdmin.getQueueInfo(queue.getName())
+        .getMessageCount();
+    assertThat(msgCount).isEqualTo(output);
+    assertThat(JdbcTestUtils.countRowsInTable(jdbcTemplate, "dog"))
+        .isZero();
+  }
+
+  @Order(4)
+  @TestCase("com.springboot.code.example.transaction.rabbitmq.testcase.TransactionTestArguments#testSendWithCloneArguments")
+  void testSendJdbcWithoutTransaction(boolean input, int output) throws Exception {
+
+    assertThatExceptionOfType(RuntimeException.class)
+        .isThrownBy(() -> producer.sendJdbcWithoutTransaction(input));
+    var msgCount = rabbitAdmin.getQueueInfo(queue.getName())
+        .getMessageCount();
+    assertThat(msgCount).isEqualTo(output);
+    assertThat(JdbcTestUtils.countRowsInTable(jdbcTemplate, "dog"))
+        .isEqualTo(output);
+  }
+
+  @Order(5)
+  @TestCase("com.springboot.code.example.transaction.rabbitmq.testcase.TransactionTestArguments#testSendArguments")
+  void testSendEntityManager(boolean input, int output) throws Exception {
+
+    assertThatExceptionOfType(RuntimeException.class)
+        .isThrownBy(() -> producer.sendEntityManager(input));
+    var msgCount = rabbitAdmin.getQueueInfo(queue.getName())
+        .getMessageCount();
+    assertThat(msgCount).isEqualTo(output);
+    assertThat(JdbcTestUtils.countRowsInTable(jdbcTemplate, "dog"))
+        .isZero();
+  }
+
+  @Order(6)
+  @TestCase("com.springboot.code.example.transaction.rabbitmq.testcase.TransactionTestArguments#testSendWithCloneArguments")
+  void testSendEntityManagerWithoutTransaction(boolean input, int output) throws Exception {
+
+    assertThatExceptionOfType(TransactionRequiredException.class)
+        .isThrownBy(() -> producer.sendEntityManagerWithoutTransaction(input));
+    var msgCount = rabbitAdmin.getQueueInfo(queue.getName())
+        .getMessageCount();
+    assertThat(msgCount)
+        .isZero();
+    assertThat(JdbcTestUtils.countRowsInTable(jdbcTemplate, "dog"))
+        .isZero();
   }
 
 }
