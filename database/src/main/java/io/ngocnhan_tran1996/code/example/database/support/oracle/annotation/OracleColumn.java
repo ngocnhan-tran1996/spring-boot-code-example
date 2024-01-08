e63b3ec79c1c64032b61c600fb53e1ae940b13b8
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2024-01-09 00:32:51 +0700

    act: update property

---
+++ database/src/main/java/io/ngocnhan_tran1996/code/example/database/support/oracle/annotation/OracleColumn.java
@@ -0,0 +1,16 @@
+package io.ngocnhan_tran1996.code.example.database.support.oracle.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OracleColumn {
+
+  String value() default "";
+
+}
--- database/src/main/java/io/ngocnhan_tran1996/code/example/database/support/oracle/mapper/DelegateOracleMapper.java
+++ database/src/main/java/io/ngocnhan_tran1996/code/example/database/support/oracle/mapper/DelegateOracleMapper.java
@@ -1,5 +1,6 @@
 package io.ngocnhan_tran1996.code.example.database.support.oracle.mapper;
 
+import java.util.function.Predicate;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -7,14 +8,13 @@ import lombok.NoArgsConstructor;
 public class DelegateOracleMapper<T> {
 
   private static final String RECORD_CLASS_NAME = "java.lang.Record";
+  private static final Predicate<Class<?>> recordClassPredicate = mappedClass -> mappedClass != null
+      && mappedClass.getSuperclass() != null
+      && RECORD_CLASS_NAME.equals(mappedClass.getSuperclass().getName());
 
   public static <T> OracleMapper<T> get(Class<T> mappedClass) {
 
-    boolean isRecord = mappedClass != null
-        && mappedClass.getSuperclass() != null
-        && RECORD_CLASS_NAME.equals(mappedClass.getSuperclass().getName());
-
-    return isRecord
+    return recordClassPredicate.test(mappedClass)
         ? RecordPropertyMapper.newInstance(mappedClass)
         : BeanPropertyMapper.newInstance(mappedClass);
   }
