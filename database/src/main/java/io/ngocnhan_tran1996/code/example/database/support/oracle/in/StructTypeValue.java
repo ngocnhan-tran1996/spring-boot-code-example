296c8b47ec0912410c3e965701d17167e3ed67aa
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2024-01-14 03:26:20 +0700

    act: change constant

--- database/src/main/java/io/ngocnhan_tran1996/code/example/database/support/oracle/in/StructTypeValue.java
+++ database/src/main/java/io/ngocnhan_tran1996/code/example/database/support/oracle/in/StructTypeValue.java
@@ -8,6 +8,8 @@ import io.ngocnhan_tran1996.code.example.database.support.oracle.utils.OracleTyp
 
 class StructTypeValue<T> extends StructArrayTypeValue<T> {
 
+  protected static final String STRUCT_TYPE = "STRUCT";
+
   StructTypeValue(List<T> values, String structTypeName) {
     super(STRUCT_TYPE, values, structTypeName);
   }
