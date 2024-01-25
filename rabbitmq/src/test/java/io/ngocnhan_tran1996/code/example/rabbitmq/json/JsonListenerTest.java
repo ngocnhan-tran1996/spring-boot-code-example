426b95cfe1214f38cf18928c84e3b1630863edfc
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2024-01-26 03:06:39 +0700

    act: add typeId

--- rabbitmq/src/test/java/io/ngocnhan_tran1996/code/example/rabbitmq/json/JsonListenerTest.java
+++ rabbitmq/src/test/java/io/ngocnhan_tran1996/code/example/rabbitmq/json/JsonListenerTest.java
@@ -1,6 +1,8 @@
 package io.ngocnhan_tran1996.code.example.rabbitmq.json;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.amqp.support.converter.AbstractJavaTypeMapper.DEFAULT_CONTENT_CLASSID_FIELD_NAME;
+import static org.springframework.amqp.support.converter.DefaultClassMapper.DEFAULT_CLASSID_FIELD_NAME;
 import java.util.List;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
@@ -9,8 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import io.ngocnhan_tran1996.code.example.container.EnableTestcontainers;
 import io.ngocnhan_tran1996.code.example.container.RabbitMQContainerInitializer;
-import io.ngocnhan_tran1996.code.example.testcase.TestCase;
 import io.ngocnhan_tran1996.code.example.rabbitmq.BaseConfig;
+import io.ngocnhan_tran1996.code.example.testcase.TestCase;
 
 @SpringBootTest(classes = {BaseConfig.class, JsonListenerConfig.class})
 @EnableTestcontainers(RabbitMQContainerInitializer.class)
@@ -31,7 +33,12 @@ class JsonListenerTest {
   @TestCase
   void testListen(Employee input, int output) throws Exception {
 
-    rabbitTemplate.convertAndSend(queue.getName(), input);
+    rabbitTemplate.convertAndSend(queue.getName(), input, message -> {
+
+      message.getMessageProperties()
+          .setHeader(DEFAULT_CLASSID_FIELD_NAME, Employee.class.getName());
+      return message;
+    });
     var msgCount = rabbitAdmin.getQueueInfo(queue.getName())
         .getMessageCount();
     assertThat(msgCount).isEqualTo(output);
@@ -40,7 +47,13 @@ class JsonListenerTest {
   @TestCase
   void testListenBatch(List<Employee> input, int output) throws Exception {
 
-    rabbitTemplate.convertAndSend(queue.getName(), input);
+    rabbitTemplate.convertAndSend(queue.getName(), input, message -> {
+
+      var props = message.getMessageProperties();
+      props.setHeader(DEFAULT_CLASSID_FIELD_NAME, List.class.getName());
+      props.setHeader(DEFAULT_CONTENT_CLASSID_FIELD_NAME, Employee.class.getName());
+      return message;
+    });
     var msgCount = rabbitAdmin.getQueueInfo(queue.getName())
         .getMessageCount();
     assertThat(msgCount).isEqualTo(output);
@@ -49,7 +62,12 @@ class JsonListenerTest {
   @TestCase
   void testReceive(Chairman input, int output) throws Exception {
 
-    rabbitTemplate.convertAndSend(queue.getName(), input);
+    rabbitTemplate.convertAndSend(queue.getName(), input, message -> {
+
+      message.getMessageProperties()
+          .setHeader(DEFAULT_CLASSID_FIELD_NAME, Chairman.class.getName());
+      return message;
+    });
     var msgCount = rabbitAdmin.getQueueInfo(queue.getName())
         .getMessageCount();
     assertThat(msgCount).isEqualTo(output);
