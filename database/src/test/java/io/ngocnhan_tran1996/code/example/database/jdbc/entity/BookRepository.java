8d92e1dbe94dbbe13baaa8b4ebb8313bf53d6723
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2024-01-26 03:08:40 +0700

    act: add jdbc

---
+++ database/src/test/java/io/ngocnhan_tran1996/code/example/database/jdbc/entity/BookRepository.java
@@ -0,0 +1,6 @@
+package io.ngocnhan_tran1996.code.example.database.jdbc.entity;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Integer> {
+}
