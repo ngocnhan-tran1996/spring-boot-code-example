8f3ca8400b82ff689dd619390eec4b312411d94c
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2024-02-09 02:18:51 +0700

    act: fix for spring 3.x

--- database/src/main/java/io/ngocnhan_tran1996/code/example/database/support/oracle/mapper/OracleMapper.java
+++ database/src/main/java/io/ngocnhan_tran1996/code/example/database/support/oracle/mapper/OracleMapper.java
@@ -4,13 +4,12 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Struct;
 import java.util.Map;
-import oracle.jdbc.OracleStruct;
 
 public interface OracleMapper<T> {
 
   Struct toStruct(Connection connection, T source, String typeName) throws SQLException;
 
-  T fromStruct(OracleStruct struct);
+  T fromStruct(Connection connection, Struct struct);
 
   T convert(Map<String, Object> source);
 
